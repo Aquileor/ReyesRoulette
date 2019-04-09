384af4434156190f80fb3727b2b143e9ca1a1b4d
Aquileo Reyes <aquileoreyes@gmail.com>
2019-04-09 08:29:26 -0400

    Run Task method inside the Main Method

---
+++ ReyesRoulette/APP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReyesRoulette
+{
+    class APP
+    {
+        //Method that starts the game and is called in the Main Method.
+        public void Task()
+        { Console.WriteLine("Welcome to Reyes Roulette. \nFist part of this game is to pic a number or Color.");
+
+           
+        }
+
+        //Method that gives you the option to play the game again or quit.
+        public void RestartGame()
+        {
+            Console.WriteLine("Do you want to play Roulette again: Y/N ?");
+
+            string game = Console.ReadLine();
+
+            if (game == "y")
+            {
+                Console.Clear();
+                Task();
+                Console.WriteLine("Select another number to play Roulette.");
+                int num = Convert.ToInt32(Console.ReadLine());
+                //Gamechoice(new);
+            }
+            else if (game != "y")
+            {
+                EndGame();
+            }
+        }
+
+
+        //Method for ending the game
+        public void EndGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Hope you had fun playing Roulette.\nPress Enter to Quit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- ReyesRoulette/Program.cs
+++ ReyesRoulette/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+Reyes Aquileo
+4/9/19
+You are to write a program that models a roulette wheel. The ball can fall randomly into
+one of 38 di erent bins.You are to calculate all the winning bets according to the bin the ball falls into.
+*/
 
+//Mathods are called into the Main Method from othere classes.
 namespace ReyesRoulette
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new APP().Task();
         }
     }
 }
